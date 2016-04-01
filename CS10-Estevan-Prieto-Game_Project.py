282c5b222ea7e2d92754bdc015def984e3d02c95
EstevanPrieto <estevanprieto6@gmail.com>
2016-03-31 20:16:17 -0700

    World Map Editing
    Added a final room to the world map and began adding the descriptions to
    the rooms.

--- CS10-Estevan-Prieto-Game_Project.py
+++ CS10-Estevan-Prieto-Game_Project.py
@@ -25,7 +25,7 @@ class Room:
 start = Room('Camp Center', 'TBA', 'n_exit', 'lab', 's_exit', 'storage')
 s_exit = Room('South Exit', 'TBA', 'start', None, 'mount', None)
 lab = Room('Laboratory', 'TBA', None, 'e_exit', 'arm', 'start')
-arm = Room('Armory', 'TBA', 'lab', None, None, None)
+arm = Room('Armory', 'TBA', 'lab', None, 'final', None)
 e_exit = Room('East Exit', 'TBA', None, 'forest', None, 'lab')
 n_exit = Room('North Exit', 'TBA', 'gates', None, 'start', None)
 farm = Room('Farm', 'TBA', None, None, 'storage', None) 
@@ -37,13 +37,17 @@ cliff = Room('Cliff', 'TBA', None, None, None, 'slope')
 peak = Room('Peak', 'TBA', 'slope', 'e_peak', None, 'w_peak')
 e_peak = Room('East Peak', 'TBA', None, None, None, 'peak')
 w_peak = Room('West Peak', 'TBA', None, 'peak', None, None)
-one = Room('Area One', 'TBA', 'one_n', 'w_exit', 'one_s', 'two')
-one_n = Room('Area One - North', 'TBA', None, None, 'one', 'two_n')
-one_s = Room('Area One - South', 'TBA', 'one', None, None, 'two_s')
-two = Room('Area Two', 'TBA', 'two_n', 'one', 'two_s', 'three')
-two_n = Room('Area Two - North', 'TBA', None, 'one_n', 'two', 'three_n')
-two_s = Room('Area Two - South', 'TBA', 'two', 'one_s', None, 'three_s')
-three = Room('Area Three', 'TBA', 'three_n', 'two', 'three_s', 'four')
+one = Room('Area One', 
+'You have crossed the bridge from your settlement into the vast desert of the Unknown. Be careful, you can easily get lost here, and we don\'t want that to happen. After all, you have to beat the game to find what is lurking in that stupid secret room.\
+ I mean, COME ON. How can you be that idiotic to forget what was in it? YOU MADE THAT ROOM. You know what? Whatever. Your own fault you lost your memory. Back to describing this room.\
+ Finding what you are looking for can be quite tricky in this area. ', 'one_n', 'w_exit', 'one_s', 'two')
+one_n = Room('Area One - North', 'There is nothing to be seen in either direction.', None, None, 'one', 'two_n')
+one_s = Room('Area One - South', 'There is nothing to be seen in either direction.', 'one', None, None, 'two_s')
+two = Room('Area Two', 'Are you even listening to what I just told you?', 'two_n', 'one', 'two_s', 'three')
+two_n = Room('Area Two - North', 'I told you there is nothing in either direction. At least listen for once in your life.', None, 'one_n', 'two', 'three_n')
+two_s = Room('Area Two - South', 'OK I AM TIRED OF YOUR- wait. Is that something glistening in the ground? Well gues what, Deadbrain? You lucked out. Again. ', 'two', 'one_s', None, 'three_s')
+three = Room('Area Three', 'I SAID THERE IS NOTHING HERE. STOP TRYING TO DIE OUT HERE. BE SMART. Oh wait. I forgot that you lack intelligence. I mean, which smart person loses their memory?', 
+'three_n', 'two', 'three_s', 'four')
 three_n = Room('Area Three - North', 'TBA', None, 'two_n', 'three', None)
 three_s = Room('Area Three - South', 'TBA', 'three', 'two_s', None, None)
 four = Room('Area Four', 'TBA', None, 'three', None, None)
@@ -64,6 +68,10 @@ ruins = Room('Village Ruins', 'TBA', None, None, None, 'boulder')
 gear = Room('The Gear', 'TBA', None, None, 'trees', 'path')
 trees = Room('Fallen Trees', 'TBA', 'gear', 'ring', None, None)
 ring = Room('Ring of Trees', 'TBA', None, None, None, 'trees')
+final = Room('The Secret Room', 'Finally. I\'m surprised someone as pathetic as you could manage to even unlock this poop hole. Are you proud of yourself? DON\'T BE. From what I can tell you, there is literally nothing here except some other person. I do not even want to know how they managed to survive here for so long without any resources. Eww, I can\'t imagine his hygiene issues. Talk about nasty. I\'ll let you talk to him.', 
+'arm', None, None, None)
+
+final == False
 
 #sets your location to the starting room in the game
 node = start 
@@ -97,6 +105,17 @@ class Dialogue:
     
     def say(self, person):
         print person.name+':"'+self.convo+'"'
+    
+    def display_options(convo):
+        print '\nYour Options:'
+        for i,p in enumerate(convo['PATHS']):
+            print '\t%d: "%s"' % (i, p[2])
+
+
+#--------------------------------------------------------------
+#ITEMS
+
+
 
 
 
